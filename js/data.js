791de1cced3a804e65a7b41e5813233dcedc2124
GintaRai <48885593+GintaRai@users.noreply.github.com>
2019-04-05 19:23:23 +0300

    Inicijavimas and Veiksmai su kintamais update

--- js/data.js
+++ js/data.js
@@ -2,37 +2,50 @@ console.log('Kintamuju inicijavimas');
 
 console.log('***1***');
 
-var number = 3;
-console.log(number);
-var number = 5;
-console.log(number);
-var number = 7;
-console.log(number);
+var number_1 = 3;
+console.log(number_1);
+var number_2 = 5;
+console.log(number_2);
+var number_3 = 7;
+console.log(number_3);
 
 console.log('***2***');
 
-var text = ('saule');
-console.log(text);
-var text = ('dangus');
-console.log(text);
-var text = ('zeme');
-console.log(text);
+var text_1 = 'saule';
+console.log(text_1);
+var text_2 = 'dangus';
+console.log(text_2);
+var text_3 = 'zeme';
+console.log(text_3);
 
 console.log('***3***');
 
-var numberArray = [1, 2, 3, 4, 5];
-console.log(numberArray);
-var numberArray = [5, 4, 3, 2, 1];
-console.log(numberArray);
-var numberArray = [1, 2, 3, 2, 1]
-console.log(numberArray);
+var numberArray_1 = [1, 2, 3, 4, 5];
+console.log(numberArray_1);
+var numberArray_2 = [5, 4, 3, 2, 1];
+console.log(numberArray_2);
+var numberArray_3 = [1, 2, 3, 2, 1]
+console.log(numberArray_3);
 
 console.log('***4***');
 
-var textArray = ['ab', 'bc', 'cd', 'de', 'ef'];
-console.log(textArray);
-var textArray = ['bla', 'bla', 'bla', 'bla', 'bla'];
-console.log(textArray);
-var textArray = ['saule', 'dangus', 'zeme', 'jura', 'pieva'];
-console.log(textArray);
+var textArray_1 = ['ab', 'bc', 'cd', 'de', 'ef'];
+console.log(textArray_1);
+var textArray_2 = ['bla', 'bla', 'bla', 'bla', 'bla'];
+console.log(textArray_2);
+var textArray_3 = ['saule', 'dangus', 'zeme', 'jura', 'pieva'];
+console.log(textArray_3);
+
+console.log('Veiksmai su kintamaisiais');
+
+console.log('***1***');
+
+/*var numbers_1_2_3_suma = number_1 + number_2 + number_3;
+console.log(numbers_1_2_3_suma);*/
+console.log(number_1 + number_2 + number_3);
+
+/*var text_1_2_3_su_tarpais = text_1 + ' ' + text_2 + ' ' + text_3;
+console.log(text_1_2_3_su_tarpais);*/
+console.log(text_1 + ' ' + text_2 + ' ' + text_3);
+
 
