43774d24a40b9cf57b992782b2f60732b67162dd
GintaRai <48885593+GintaRai@users.noreply.github.com>
2019-04-05 16:32:27 +0300

    Kintamuju inicijavimas update

---
+++ js/data.js
@@ -0,0 +1,38 @@
+console.log('Kintamuju inicijavimas');
+
+console.log('***1***');
+
+var number = 3;
+console.log(number);
+var number = 5;
+console.log(number);
+var number = 7;
+console.log(number);
+
+console.log('***2***');
+
+var text = ('saule');
+console.log(text);
+var text = ('dangus');
+console.log(text);
+var text = ('zeme');
+console.log(text);
+
+console.log('***3***');
+
+var numberArray = [1, 2, 3, 4, 5];
+console.log(numberArray);
+var numberArray = [5, 4, 3, 2, 1];
+console.log(numberArray);
+var numberArray = [1, 2, 3, 2, 1]
+console.log(numberArray);
+
+console.log('***4***');
+
+var textArray = ['ab', 'bc', 'cd', 'de', 'ef'];
+console.log(textArray);
+var textArray = ['bla', 'bla', 'bla', 'bla', 'bla'];
+console.log(textArray);
+var textArray = ['saule', 'dangus', 'zeme', 'jura', 'pieva'];
+console.log(textArray);
+
